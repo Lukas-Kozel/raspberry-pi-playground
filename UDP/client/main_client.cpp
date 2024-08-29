40caa673d7ec3137a848b52c1f473938e656339b
Lukas-Kozel <l.kozel@outlook.cz>
2024-08-29 21:30:54 +0200

    minor update, client and server side switched to smart pointers for better memory management.

--- UDP/client/main_client.cpp
+++ UDP/client/main_client.cpp
@@ -1,38 +1,44 @@
 //< raspberry Pi main app.
 // purpose of this main component is to recieve data from other client components,
 // validate them and send HB, that will contain status or error code of all client components 
-
+#pragma once
 #include "Multicast_sender.h"
 #include "msg/heartbeat.h"
 #include "utils/utils.h"
+#include "utils/logger.h"
 #include <chrono>
 #include <thread>
 #include <vector>
+
+Logger logger("/home/lukas-kozel/raspberry-pi-playground/UDP/server/Configuration/config.conf");
+
 
 int main(int argc, char* argv[]) {
     ArgParser argParser;
     parse_arguments(argc,argv,argParser);
     const std::string multicast_ip = "224.0.0.1";
     const int port = 8081;
-
-    MulticastSender sender(multicast_ip, port);
+    Heartbeat heartbeat;
+    std::unique_ptr<MulticastSender> sender = std::make_unique<MulticastSender>(multicast_ip, port);
     if(argParser.debug) std::cout << "loaded" << std::endl;
     std::thread hb_thread([&](){
         if(argParser.debug) std::cout << "thread created" << std::endl;
-    Heartbeat heartbeat;
+
 
     while (true) {
         heartbeat.update_timestamp();
         heartbeat.update_life_counter();
         std::vector<uint8_t> heartbeatMessage(heartbeat.size());
         heartbeat.serialize(heartbeatMessage.data());
-        sender.sendMulticast(heartbeatMessage);
-        if(argParser.debug) std::cout << "time_ms = " << heartbeat.timestamp_ms 
+        sender->sendMulticast(heartbeatMessage);
+        std::ostringstream oss;
+        if(argParser.debug) oss << "time_ms = " << heartbeat.timestamp_ms 
         << " ; time_ns = " << heartbeat.timestamp_ns 
         << " ; life_counter = " << heartbeat.life_counter
         << " ; error_code = " << (int)heartbeat.error_code 
         << " ; status = " << (int)heartbeat.status 
         << std::endl;
+        logger.log(DEBUG,oss.str()+"\n");  
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
     });
--- UDP/server/main_server.cpp
+++ UDP/server/main_server.cpp
@@ -1,6 +1,6 @@
 //< server main app.
 // purpose of this main server component is to validate HB, send commands via GUI and keep track of current status of client.
-
+#pragma once
 #include "multicast_receiver.h"
 #include "msg/heartbeat.h"
 #include "utils/utils.h"
@@ -18,7 +18,8 @@ enum HeartbeatValidationError {
     HB_ERROR_CODE           = 0b00000001, // Error code is non-zero
     HB_ERROR_STATUS         = 0b00000010, // Status is not OK
     HB_ERROR_TIMESTAMP      = 0b00000100, // Timestamp is invalid
-    HB_ERROR_LIFE_COUNTER   = 0b00001000  // Life counter is invalid
+    HB_ERROR_LIFE_COUNTER   = 0b00001000,  // Life counter is invalid
+    HB_CHECK_SUM_ERROR      = 0b00010000  // checksum invalid
 };
 uint8_t hb_error_flags = HB_ERROR_NONE;
 
@@ -26,7 +27,7 @@ uint8_t hb_error_flags = HB_ERROR_NONE;
 uint32_t previous_life_counter = 0;
 uint8_t tolerance = 100; // in ms
 uint64_t timestamp_ms_previous=static_cast<uint64_t>( std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
-Logger logger("./Configuration/config.conf");
+Logger logger("/home/lukas-kozel/raspberry-pi-playground/UDP/server/Configuration/config.conf");
 
 bool validate_hb_msg(Heartbeat *hb);
 bool validate_timestamp(uint64_t timestamp_ms_current, uint64_t tolerance);
@@ -38,30 +39,31 @@ int main(int argc, char* argv[]) {
 
     const std::string multicast_ip = "224.0.0.1";
     const int port = 8081;
-    MulticastReceiver receiver(multicast_ip, port);
+    std::unique_ptr<MulticastReceiver> receiver = std::make_unique<MulticastReceiver>(multicast_ip, port);
     std::vector<uint8_t> heartbeatMessage(1024);
-
+    Heartbeat hb_msg;
     if(argParser.debug) std::cout << "loaded" << std::endl;
     std::thread hb_thread([&](){    
         if(argParser.debug) std::cout << "thread created" << std::endl;
         while (true){
 
-            receiver.receive(heartbeatMessage);
+            receiver->receive(heartbeatMessage);
 
             if (!heartbeatMessage.empty()) {
-                Heartbeat hb = Heartbeat::deserialize(heartbeatMessage.data());
-                if(!validate_hb_msg(&hb)){
+                hb_msg.deserialize(heartbeatMessage.data());
+                if(!validate_hb_msg(&hb_msg)){
                     std::cerr << "Heartbeat message is not valid" << std::endl;
+                    logger.log(ERROR,"Heartbeat message is not valid");
                     //exit(EXIT_FAILURE);
                 } 
-                previous_life_counter = hb.life_counter;
-                timestamp_ms_previous = hb.timestamp_ms;
+                previous_life_counter = hb_msg.life_counter;
+                timestamp_ms_previous = hb_msg.timestamp_ms;
                 std::ostringstream oss;
-                oss << "time_ms = " << hb.timestamp_ms 
-                        << " ; time_ns = " << hb.timestamp_ns 
-                        << " ; life_counter = " << hb.life_counter
-                        << " ; error_code = " << (int)hb.error_code 
-                        << " ; status = " << (int)hb.status; 
+                oss << "time_ms = " << hb_msg.timestamp_ms 
+                        << " ; time_ns = " << hb_msg.timestamp_ns 
+                        << " ; life_counter = " << hb_msg.life_counter
+                        << " ; error_code = " << (int)hb_msg.error_code 
+                        << " ; status = " << (int)hb_msg.status; 
                 logger.log(DEBUG,oss.str()+"\n");                 
                 if(argParser.debug) std::cout << oss.str() << std::endl;
             } else {
@@ -77,6 +79,7 @@ int main(int argc, char* argv[]) {
 }
 
 bool validate_hb_msg(Heartbeat *hb){ //TODO: refactor this to version via binary ok steps to determine, which of those is not ok. 
+    //bool checksum_ok = checksum(sizeof(hb),heartbeatMessage);
     bool error_code_ok = hb->error_code ==0;
     bool status_ok = hb->status == OK_STATUS;
     bool timestamp_ok = validate_timestamp(hb->timestamp_ms, tolerance);
@@ -97,7 +100,7 @@ bool validate_hb_msg(Heartbeat *hb){ //TODO: refactor this to version via binary
         hb_error_flags |= HB_ERROR_LIFE_COUNTER;
         return false;
     }
-
+    return false;
 }
 
 bool validate_timestamp(uint64_t timestamp_ms_current, uint64_t tolerance) {
@@ -113,6 +116,10 @@ bool validate_life_counter(uint32_t life_counter){
     }
     return false;
 }
+
+// bool checksum(size_t received_size,size_t expected_size){
+//     return (received_size == expected_size);
+// }
 
 void check_and_report_failure(){
     if(hb_error_flags != HB_ERROR_CODE){
