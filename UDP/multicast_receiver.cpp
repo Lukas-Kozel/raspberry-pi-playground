5edd940ef643085b35f982e9494dbfd081f0b56e
Lukas-kozel <l.kozel@outlook.cz>
2024-08-30 20:24:07 +0200

    switched to listen on UDP port in its own thread

--- UDP/multicast_receiver.cpp
+++ UDP/multicast_receiver.cpp
@@ -3,6 +3,35 @@
 void MulticastReceiver::receiveMulticast(std::vector<uint8_t> &message)
 {
     receive(message);
+    if (callback && !message.empty()) { //msg is not empty and callback is set.
+        callback(message);
+    }
+    else if(message.empty()){
+        //TODO log
+    }
 }
 
 MulticastReceiver::MulticastReceiver(const std::string& multicast_ip, int port): UDPClient(multicast_ip,port){};
+
+MulticastReceiver::~MulticastReceiver() {
+    if (listening_thread.joinable()) {
+        listening_thread.join(); // Clean up the thread
+    }
+}
+
+void MulticastReceiver::registerCallback(std::function<void(const std::vector<uint8_t> &)> callback)
+{
+    this->callback = callback;
+    this->listening_thread = std::thread(&MulticastReceiver::listen_for_message,this);
+}
+
+void MulticastReceiver::listen_for_message()
+{
+    std::vector<uint8_t> buffer(1024);
+    while(true){
+        receive(buffer);
+        if(!buffer.empty() && this->callback){
+            this->callback(buffer);
+        }
+    }
+}
--- UDP/multicast_receiver.h
+++ UDP/multicast_receiver.h
@@ -2,11 +2,21 @@
 #define MULTICASTRECEIVER_H
 #pragma once
 #include "client.h"
+#include <functional>
+#include <thread>
 
 class MulticastReceiver : public UDPClient {
 public:
     MulticastReceiver(const std::string& multicast_ip, int port);
     void receiveMulticast(std::vector<uint8_t>& message);
+    void registerCallback(std::function<void(const std::vector<uint8_t>&)> callback);
+    void listen_for_message();
+
+private:
+    std::function<void(const std::vector<uint8_t>&)> callback;
+    std::thread listening_thread;
+    MulticastReceiver();
+    ~MulticastReceiver();
 };
 
 #endif // MULTICASTRECEIVER_H
